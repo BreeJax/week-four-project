eb06ee1df46751e1ff3e6f51db11c421496b3ae0
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-08-01 16:59:47 -0400

    random word added and make into _

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mustacheExpress = require("mustache-express")
 const bodyParser = require("body-parser")
 const app = express()
 const expressValidator = require("express-validator")
+const expressSession = require("express-session")
 const fs = require("fs") //is a built in mod that comes for free with node but is not require by default
 
 app.engine("mustache", mustacheExpress())
@@ -12,14 +13,33 @@ const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().sp
 app.set("views", "./templates")
 app.set("view engine", "mustache")
 app.use(express.static("public"))
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
-  res.render("index")
+  let randomWord = words[Math.floor(Math.random() * words.length)]
+  let PLACEHOLDER = "_"
+  let hiddenWord = randomWord
+    .split("")
+    .map(letter => {
+      return PLACEHOLDER
+    })
+    .join("")
+
+  //where word is chosen
+  //make underscores
+
+  res.render("index", { hiddenWord: hiddenWord })
+})
+//where would I send people are guess is wrong? post?
+//where do I show wrong guesses
+
+app.post("/", (req, res) => {
+  //wher the guesses go
+  //where you put the page that tells people if they win
+  //where you put the page that tells people if they lose
 })
 
 app.listen(3000, () => {
-  console.log(words)
   console.log("I've got the magic in me!")
 })
