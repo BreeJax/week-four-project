683fa58b058d459e77db71e6a8f7077934e5dcfb
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-08-01 19:18:54 -0400

    word made stable and guesses are working ish

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express")
 const mustacheExpress = require("mustache-express")
 const bodyParser = require("body-parser")
-const app = express()
 const expressValidator = require("express-validator")
 const expressSession = require("express-session")
 const fs = require("fs") //is a built in mod that comes for free with node but is not require by default
+
+const app = express()
+
+app.use(
+  expressSession({
+    secret: "NoLove",
+    resave: false,
+    saveUninitialized: true
+  })
+)
 
 app.engine("mustache", mustacheExpress())
 
@@ -17,27 +26,40 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
-  let randomWord = words[Math.floor(Math.random() * words.length)]
+  const letterGuess = req.session.letterGuess || [] //part of sessions
+  //where random word is chosen
+  if (!req.session.randomWord) {
+    req.session.randomWord = words[Math.floor(Math.random() * words.length)]
+  }
+  let randomWord = req.session.randomWord
   let PLACEHOLDER = "_"
   let hiddenWord = randomWord
     .split("")
     .map(letter => {
-      return PLACEHOLDER
+      return letterGuess.includes(letter) ? letter : PLACEHOLDER
     })
     .join("")
 
-  //where word is chosen
-  //make underscores
-
-  res.render("index", { hiddenWord: hiddenWord })
+  res.render("index", { hiddenWord: hiddenWord, letterGuessed: req.session.letterGuess })
 })
 //where would I send people are guess is wrong? post?
 //where do I show wrong guesses
 
-app.post("/", (req, res) => {
-  //wher the guesses go
-  //where you put the page that tells people if they win
-  //where you put the page that tells people if they lose
+// app.post("/", (req, res) => {
+//   //wher the guesses go
+//   //where you put the page that tells people if they win
+//   //where you put the page that tells people if they lose
+// })
+
+app.post("/guess", (req, res) => {
+  const letterGuess = req.session.letterGuess || []
+  const randomWord = req.session.randomWord || []
+
+  // letters.push({ id: todos.length + 1, completed: false, description: description })
+  letterGuess.push(req.body.letterGuess)
+  req.session.letterGuess = letterGuess
+
+  res.redirect("/")
 })
 
 app.listen(3000, () => {
